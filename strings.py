cbcdb0a31f54ddf60b66682741c02036b6fd7b94
froyge <43520680+froyge@users.noreply.github.com>
2022-02-12 00:23:24 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,36 @@
+START_MSG = """
+Hello <b>{}</b> ! I'm <b>Akinator</b>.
+Let's play a guessing game. Think about your favorite character. I will try to guess who it is.
+Type <code>/play</code>
+"""
+
+AKI_LANG_CODE = {
+    'en': 'English',
+    'ar': 'Arabic',
+    'cn': 'Chinese',
+    'de': 'German',
+    'es': 'Spanish',
+    'fr': 'French',
+    'il': 'Hebrew',
+    'it': 'Italian',
+    'jp': 'Japanese',
+    'kr': 'Korean',
+    'nl': 'Dutch',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'ru': 'Russian',
+    'tr': 'Turkish',
+    'id': 'Indonesian',
+}
+
+AKI_LANG_MSG = """ 
+Change Playing Language. <b>NOTE : This does not change the Bot language.</b>
+<b>Current Language :</b> <pre>{}</pre>
+"""
+
+CHILDMODE_MSG = """
+If Child mode is enabled, akinator won't show any NSFW content.
+<b>Current Status :</b> <pre>Child mode is {} !</pre>
+"""
+
+AKI_FIRST_QUESTION = "This is the first question. You can't go back any further!"
